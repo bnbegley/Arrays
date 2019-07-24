4f32d4c37adc271ad945e41002407d1c5ca3cdd9
brail33na11@gmail.com <brail33na11@gmail.com>
2019-07-24 16:34:31 -0400

    Add project files.

---
+++ ArraysProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ArraysProject {
+    class Program {
+        static void Main(string[] args)
+        {
+            int[] frames = new int[10];
+
+            frames[0] = 30;
+            frames[1] = 30;
+            frames[2] = 27;
+            frames[3] = 30;
+            frames[4] = 30;
+            frames[5] = 25;
+            frames[6] = 30;
+            frames[7] = 30;
+            frames[8] = 27;
+            frames[9] = 30;
+
+            int total = 0;
+            int index = 0;
+            while (index < 10)
+            {
+                total = total + frames[index];
+                index = index + 1;
+            }
+            Console.WriteLine($"Total is {total}.");
+        }
+    }
+}
